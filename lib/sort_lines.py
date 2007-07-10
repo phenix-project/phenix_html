fd43e5ba664e11a78cc55f1641a550c7b43fc3f5
Tom Terwilliger <tterwilliger@newmexicoconsortium.org>
2007-07-10 19:36:24 +0000

    Add sorting script for reject and two_word

---
+++ lib/sort_lines.py
@@ -0,0 +1,16 @@
+import sys
+
+def run(args):
+  def sort_it(inp):
+    lines = inp.read().splitlines()
+    lines.sort()
+    for line in lines:
+      print line
+  if (len(args) == 0):
+    sort_it(inp=sys.stdin)
+  else:
+    for file_name in args:
+      sort_it(inp=open(file_name))
+
+if (__name__ == "__main__"):
+  run(sys.argv[1:])
