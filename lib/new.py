2f304f5e5f7f93132fd192534937b68d78901323
Tom Terwilliger <tterwilliger@newmexicoconsortium.org>
2007-07-22 17:12:25 +0000

    Update reject list for index

--- lib/new.py
+++ lib/new.py
@@ -1,7 +1,13 @@
+#cat ../phenix_index.htm | grep '<LI>' | grep -v '<LI><A' > list1.dat
+all_lines=[]
+for line in open('../phenix_index.htm').readlines():
+  if line and line.find("<LI>")>-1 and not line.find("<LI><A")>-1:
+    all_lines.append(line.replace("<LI>","").replace("\n",""))
+
 text=open("good_list").read()
 good_lines=text.split("\n")
-text=open("list1.dat").read()
-all_lines=text.split("\n")
+f=open('all_list','w')
 for line in all_lines:
+  f.write(line+"\n")
   if line and line.replace(" ","") and not line in good_lines:
      print line
